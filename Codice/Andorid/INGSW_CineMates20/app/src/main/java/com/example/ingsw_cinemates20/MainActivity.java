4abf454033982e98c85fad9efaf84756d99c1e11
Franck024 <fcozzolino24@gmail.com>
2021-04-28 21:48:52 +0200

    schermata login
    -Aggiornata la schermata login
    -Aggiunte icone android al progetto

--- Codice/Andorid/INGSW_CineMates20/app/src/main/java/com/example/ingsw_cinemates20/MainActivity.java
+++ Codice/Andorid/INGSW_CineMates20/app/src/main/java/com/example/ingsw_cinemates20/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.ingsw_cinemates20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
+// sto cazzeggiando con i bottoni
+        Button evento = (Button) findViewById(R.id.buttonLogin);
+        evento.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Button y = (Button) findViewById(R.id.buttonLogin);
+                y.setText("OK");
+            }});
+
+
+
     }
+
+
+
+
 }
+
+
