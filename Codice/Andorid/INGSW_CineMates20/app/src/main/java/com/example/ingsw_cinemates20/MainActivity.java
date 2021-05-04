f98a1b052f1e4d94e5986f1a1a46f52a4c9e9899
Franck024 <fcozzolino24@gmail.com>
2021-05-04 19:42:43 +0200

    Aggiornamento login
    schermata Login aggiornata

--- Codice/Andorid/INGSW_CineMates20/app/src/main/java/com/example/ingsw_cinemates20/MainActivity.java
+++ Codice/Andorid/INGSW_CineMates20/app/src/main/java/com/example/ingsw_cinemates20/MainActivity.java
@@ -1,53 +1,26 @@
 package com.example.ingsw_cinemates20;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    TabLayout tabLayout;
-    ViewPager viewPager;
-    FloatingActionButton fb, google;
-    float v=0;
+    TabLayout panel;
+    EditText postalAddressEmail, passwordPass, textViewRecovery, textViewSignUp, textView;
+    Button buttonLogin, buttonLoginFB, buttonLoginGG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginv3);
+        setContentView(R.layout.login);
 
 
-        tabLayout = findViewById(R.id.tab_layout);
-        viewPager = findViewById(R.id.view_pager);
-        fb = findViewById(R.id.fab_fb);
-        google = findViewById(R.id.fab_google);
-
-        tabLayout.addTab(tabLayout.newTab().setText(""));
-        tabLayout.addTab(tabLayout.newTab().setText("Signup"));
-        tabLayout.setTabGravity(tabLayout.GRAVITY_FILL);
-
-
-        // getTabCount() fa crashare il programma!
-       final LoginAdapter adapter = new LoginAdapter(getSupportFragmentManager(),this, tabLayout.getTabCount());
-        viewPager.setAdapter(adapter);
-
-
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-
-        fb.setTranslationY(300);
-        google.setTranslationY(300);
-        tabLayout.setTranslationY(300);
-
-        fb.setAlpha(v);
-        google.setAlpha(v);
-        tabLayout.setAlpha(v);
-
-        fb.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
-        google.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+    }
 
     /*
         Button evento = (Button) findViewById(R.id.buttonLogin);
@@ -63,6 +36,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-}
+
 
 
---
+++ Codice/Andorid/INGSW_CineMates20/app/src/main/java/com/example/ingsw_cinemates20/login/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.ingsw_cinemates20.login;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ingsw_cinemates20.R;
+
+public class LoginController extends AppCompatActivity {
+    EditText postalAddressEmail, passwordPass, textViewRecovery, textViewSignUp, textView;
+    Button buttonLogin, buttonLoginFB, buttonLoginGG;
+
+ }
